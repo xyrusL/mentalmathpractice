77c654b2d72c81161c170fbbb5e36ea523063843
Xyrus <193227036+lzdMk@users.noreply.github.com>
2025-04-04 15:55:00 +0800

    Update pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, useState } from 'react-router-dom'
+import React, { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
